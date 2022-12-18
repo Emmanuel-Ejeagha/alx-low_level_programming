60f2e445e9d33120680368626278a0cc581d539e
Emmanuel-Ejeagha <nuel6700@gmail.com>
2022-12-18 20:34:42 +0000

    task 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,8 +11,10 @@ void print_alphabet_x10(void)
 	while (count++ <= 9)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
+		{
 			_putchar(letter);
-	_putchar('n,);
+		}
+	_putchar('\n');
 	}
-	return (0);
+	
 }
