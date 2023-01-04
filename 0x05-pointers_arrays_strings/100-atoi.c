6dc15b9ea90e35959a3810d2d5d64ccea18b884f
Emmanuel-Ejeagha <nuel6700@gmail.com>
2023-01-04 14:42:22 +0000

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,33 +8,39 @@
  */
 int _atoi(char *s)
 {
-	int c = 0;
-	unsigned int ni = 0;
-	int min = 1;
-	int isi = 0;
+	int i, d, len, f, digit;
 
-	while (s[c])
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++
+
+	while (i < len && f == '\0')
 	{
-		if (s[c] == 45)
-		{
-			min *= -1;
-		}
+		if (s[i] == '-')
+			++d;
 
-		while (s[c] >= 48 && s[c] <= 57)
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			isi = 1;
-			ni = (ni * 10) + (s[c] - '0');
-			c++;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
-
-		if (isi == 1)
-		{
-			break;
-		}
-
-		c++
+		i++;
 	}
-
-	ni *= min;
-	return (ni);
+	if (f == 0)
+		return (0);
+	return (n);
 }
