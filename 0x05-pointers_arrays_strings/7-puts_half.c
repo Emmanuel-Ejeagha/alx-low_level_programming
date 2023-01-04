1c5f290df543e7f2ca0d8f3adcfe08e1788e558f
Emmanuel-Ejeagha <nuel6700@gmail.com>
2023-01-04 10:24:52 +0000

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,17 +7,17 @@
  */
 void puts_half(char *str)
 {
-	int string = 0, i;
+	int string = 0, n;
 
 	while (str[string] != '\0')
 		string++;
 	if (string + 1 % 2 != '0')
-		i = (string - 1) / 2;
+		n = (string - 1) / 2;
 	else
-		i = (string / 2);
+		n = (string / 2);
 	i++;
 
-	for (string = i; str[string] != '\0'; string++)
+	for (string = n; str[string] != '\0'; string++)
 		_putchar(str[string]);
 	_putchar('\n');
 }
