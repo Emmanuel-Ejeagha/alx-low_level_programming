76ab4f0365a1ef65bfb3f5be09508bff37efa516
Emmanuel-Ejeagha <nuel6700@gmail.com>
2023-01-04 21:56:09 +0000

    pointers

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,23 +1,11 @@
 #include "main.h"
 /**
- * _memcpy - a function that copies memory area
- * @dest: memory where is stored
- * @src: memory where is copied
- * @n: number of bytes
- *
- * Return: copied memory with n byted changed
- */
+ *  * set_string - sets the value of a pointer to a char
+ *   * @s: pointer to pointer
+ *    * @to: pointer char
+ *     */
 
-char *_memcpy(char *dest, char *src, unsigned int n)
+void set_string(char **s, char *to)
 {
-	int r = 0;
-	int i = n;
-
-	for (; r < i; r++)
-	{
-		dest[r] = src[r];
-
-		n--;
-	}
-	return (dest);
+	*s = to;
 }
