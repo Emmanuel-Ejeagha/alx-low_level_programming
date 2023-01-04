46e732bd684eb8609e68797799c3a2436d15ac6e
Emmanuel-Ejeagha <nuel6700@gmail.com>
2023-01-04 08:35:53 +0000

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - fuction that prints every other char of a string
+ * @str: char to check
+ *
+ * Return: Always 0
+ */
+void puts2char(char *str)
+{
+	int string;
+
+	for (string = 0; str[string] != '\0'; string++)
+
+		if (string % 2 == 0)
+		{
+			_putchar(str[string];
+		}
+		_putchar('\n')
+}
