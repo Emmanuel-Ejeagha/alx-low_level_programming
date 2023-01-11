bf95743aec7d08ca3f47814b4d144ca74ce6ca2a
Emmanuel-Ejeagha <nuel6700@gmail.com>
2023-01-11 19:27:05 +0000

    task one

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * int main - program that prints its name followed by a newline
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 Always
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
