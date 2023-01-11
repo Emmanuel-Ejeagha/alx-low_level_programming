740dc8c9e85486155b0d66151bce2d11e64c5042
Emmanuel-Ejeagha <nuel6700@gmail.com>
2023-01-11 20:09:03 +0000

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: argument vector
  * Return: 0
  */
-int main(int argc, char **argv[]__attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
