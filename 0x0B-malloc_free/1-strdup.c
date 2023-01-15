7f595b7f8c2d796075873fce89ffd0ca042e7ff7
Emmanuel-Ejeagha <nuel6700@gmail.com>
2023-01-15 23:17:35 +0000

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,26 +9,29 @@
 char *_strdup(char *str)
 {
 	char *duplicate;
-	int i, j;
+	int i, j = 0;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; str[i] != '\0';)
-		;
+	while (str[i] != '\0';)
+		j++;
 
-	duplicate = (char *)malloc(sizeof(char) * (i + 1));
+
+	duplicate = (char *)malloc((sizeof(char) * j) + 1);
 
 	if (duplicate == NULL)
 	{
 		return (NULL);
 	}
 
-	for (j = 0; j <= i; j++)
+	for (i = 0; i <= j; i++)
 	{
-		duplicate[j] = str[j];
+		duplicate[i] = str[i];
 	}
+	copy[j] = '\0';
+
 	return (duplicate);
 }
