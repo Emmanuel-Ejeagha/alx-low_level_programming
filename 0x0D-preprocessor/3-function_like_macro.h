6a8d21508c67961fee7a379fe7af6ca07c868dd8
Emmanuel-Ejeagha <nuel6700@gmail.com>
2023-01-18 03:30:59 +0000

    0x0D. C - Preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
 
-#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
 
-#endif 
+#endif
